faeb9646c68b05521d027bc578d84985259f0fce
Nikos Metaxakis <metaxakisn@gmail.com>
2020-10-26 18:50:22 +0200

    added a turn information component with its accompaning classes

---
+++ src/components/play-symbol.js
@@ -0,0 +1,12 @@
+import { WritableComponent } from "../libs/writable-component";
+
+/*Much like CurrentTurn, it shows the symbol that is playing now*/
+export class PlaySymbol extends WritableComponent {
+    constructor() {
+        super("symbol");
+        this.element.setAttribute("style", "float: right");
+    }
+    set textContent(v) {
+        super.textContent = `playing: ${v}`;
+    }
+}
